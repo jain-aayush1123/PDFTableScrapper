c3b30b1e22f28cf5c3f266f202c7d4abc51716ac
awsm1123 <jaayushmax@gmail.com>
2019-12-17 15:49:58 +0530

    intial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import pdf_table_scrapper
+
+scrapper = pdf_table_scrapper.PDFTableScrapper()
+scrapper.tear_pdf('timetable.pdf')
+scrapper.parse_pdf_to_csv(6, 10, 'converted_timetable.csv')
---
+++ pdf_table_scrapper.py
@@ -0,0 +1,42 @@
+import tabula
+import pandas
+import os
+import PyPDF2
+
+
+class PDFTableScrapper:
+    def __init__(self):
+        super().__init__()
+
+    def tear_pdf(self, pdf_name):
+        # tear the pdf into individual pages
+        pdf_file = open(pdf_name, 'rb')
+        read_pdf = PyPDF2.PdfFileReader(pdf_file)
+        number_of_pages = read_pdf.getNumPages()
+        for page in range(number_of_pages):
+            p = read_pdf.getPage(page)
+            write_file = PyPDF2.PdfFileWriter()
+            write_file.addPage(p)
+
+            output = 'Pages/%02d.pdf' % page
+            with open(output, 'wb') as f:
+                write_file.write(f)
+
+    def parse_pdf_to_csv(self, starting_page, ending_page, output_file):
+        # parse data
+        split_pdf_files = os.listdir("Pages")
+        split_pdf_files.sort()
+        for file in split_pdf_files:
+            page_no = int(file.split('.')[0])
+            if(page_no >= starting_page and page_no <= ending_page):
+                data = tabula.read_pdf(
+                    'Pages/' + file,
+                    pandas_options={
+                        'header': 0,
+                        'keep_default_na': False,
+                        'usecols': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12]
+                    }
+                )
+                csv_file = open(output_file, 'a')
+                data.to_csv(csv_file, index=None)
+                print("done: " + str(page_no))
